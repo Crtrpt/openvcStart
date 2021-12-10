6b97b39d54f3cefed197f8fc4aa6b0ff2b562550
jzx <945627077@qq.com>
2021-12-10 17:04:45 +0800

    '测试cmakeJNI'

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,15 @@
-import org.opencv.core.Core;
-import org.opencv.core.CvType;
-import org.opencv.core.Mat;
-import org.opencv.highgui.HighGui;
-import org.opencv.imgcodecs.Imgcodecs;
-import org.opencv.imgproc.Imgproc;
-import org.opencv.osgi.OpenCVInterface;
+
 
 public class Main {
+
+    static {
+        System.load("D:\\private\\openvcstart\\src\\main\\cpp\\build\\Debug\\HelloJNI.dll");
+    }
+
+
+    private native void sayHello();
+
     static public void  main(String[] argv){
-        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-
-
-        //Reading the Image from the file
-        Mat src = Imgcodecs.imread("./test.png");
-        HighGui.imshow("", src);
-        //Converting color to gray scale
-        Mat gray = new Mat(src.rows(), src.cols(), src.type());
-        Imgproc.cvtColor(src, gray, Imgproc.COLOR_RGB2GRAY);
-        HighGui.imshow("Gray Scale Image", gray);
-        //Applying canny
-        Mat dst = new Mat(src.rows(), src.cols(), src.type());
-        Imgproc.Canny(gray, dst, 100, 100*3);
-        HighGui.imshow("Edges", dst);
-        HighGui.waitKey();
+        System.out.println("hellojni");
     }
 }
